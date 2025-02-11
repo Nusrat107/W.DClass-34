36da6a5b478fc82f5285a7ba98a036d2c22a64ad
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-02-11 11:48:56 +0600

    Class-34

---
+++ script.js
@@ -0,0 +1,40 @@
+//Write a js program to find positive number .\
+
+ let number1 = -32;
+
+
+if ( number1 > 0){
+    console.log ("This is a positive number");
+}
+
+else {
+    console.log ("The number is negative");
+}
+
+
+
+//Write a js program to find maximum between two numbers.\
+
+let number3 = 59;
+let number4 = 42;
+
+if(number3 > number4){
+    console.log ("The maximum number is 59");
+}
+
+else if(number3 < number4){
+    console.log ("The maximum nuber is 42");
+}
+
+
+//write a js program to chech whether a number is divisible by 5.
+
+let number5 = 5;
+
+if (number5 % 5 == 0){
+    console.log ("This number is divisible by 5")
+}
+
+else{
+    console.log ("This number is not divisible by 5")
+}
